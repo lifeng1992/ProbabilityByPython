10242b66afc0956c3b7d0e34fd13930941551096
lifeng1992 <lifeng@bamcop.com>
2015-05-14 21:13:23 +0800

    Refactoring
    Refactoring to make code more python

--- MADIO-8-B/PanDecrypt.py
+++ MADIO-8-B/PanDecrypt.py
@@ -1,121 +1,89 @@
-import random, enchant, itertools
+import itertools, enchant
 
 class PanDecrypt:
-	"""docstring for Bdecrypt"""
-	perhaps		= 'etaoinshrdlcumwfgypbvkjxqz'
-	P   		= 'abcdefghijklmnopqrstuvwxyz'
-	ciphertext	= ''
-	letterDict	= {}
-	wordsList	= ()
-	init_key	= ''
-	length		= 6
-
-	def __init__(self):
-		f = open('0x01-ciphertext.txt', 'r')
-		self.ciphertext = f.read()
-		f.close()
-		pass
-
-	def run(self):
-		self.count_letter()
-		self.get_wordList()
-		self.get_init_key()
-		pass
-
-	def check_init_key(self):
-		print('init_key is : ' + self.init_key)
-		print('P(init_key) : ' + str(self.check(self.init_key)))
-
-	def count_letter(self):
-		for c in self.ciphertext:
-			if c > 'z' or c < 'a':
-				pass
-			elif c in self.letterDict:
-				self.letterDict[c] += 1
-			else:
-				self.letterDict.setdefault(c, 1)
-
-	def all_is_letter(self, s):
-		for c in s:
-			if c > 'z' or c < 'a':
-				return False
-
-		return True
-
-	def get_wordList(self):
-		strs = self.ciphertext.split()
-
-		for s in strs:
-			if self.all_is_letter(s):
-				if s not in self.wordsList:
-					self.wordsList = self.wordsList + (s, )
-
-	def get_init_key(self):
-		letterList	= sorted(self.letterDict.items(), key = lambda e : e[1] , reverse=True)
-		
-		for x in range(len(letterList)):
-			self.init_key = self.init_key + letterList[x][0]
-
-	def check(self, trans):
-		table = str.maketrans(trans, self.perhaps)
-
-		T = enchant.Dict('en_US')
-		succ_perhaps = 0 
-
-		for x in self.wordsList:
-			if T.check(x.translate(table)):
-				succ_perhaps += 1
-
-		return succ_perhaps * 1.0 / len(self.wordsList)
-
-
-	def upgrade(self, str_statrt, str_middle, str_end):
-		middle_list = sorted(str_middle)
-		middle_strs = ''
-		for i in range(len(middle_list)):
-			middle_strs = middle_strs + middle_list[i]
-
-		best_succ = 0
-		best_strs = ''
-		for x in list(itertools.permutations(middle_strs)):
-			strs = ''
-			for i in range(len(x)):
-				strs = strs + x[i]
-
-			if self.check(str_statrt + strs + str_end) > best_succ:
-				best_succ = self.check(str_statrt + strs + str_end)
-				best_strs = strs
-				print(best_strs + ' ' + str(best_succ))
-
-		return best_strs
-
-
-	def auto_decrypt(self):
-		best_pwd  = self.init_key
-		best_suc  = self.check(self.init_key)
-
-		for i in range(len(self.init_key) - self.length + 1):
-			tstrs_start		= best_pwd[0: i]
-			tstrs_middle 	= best_pwd[i: i + self.length]
-			tstrs_end		= best_pwd[i + self.length:]
-			
-			print('The ' + str(i) + ' times:' + '\n')
-			print(tstrs_start + ' ' + tstrs_middle + ' ' + tstrs_end + '\n')
-
-			upgrade_middle = self.upgrade(tstrs_start, tstrs_middle, tstrs_end)
-			best_pwd = tstrs_start + upgrade_middle + tstrs_end
-			best_suc = 0
-
-		return best_pwd
-
+    """docstring for PanDecrypt"""
+    perhaps 	= 'etaoinshrdlcumwfgypbvkjxqz'
+    P 		= 'abcdefghijklmnopqrstuvwxyz'
+    
+    def __init__(self, ciphertext, length = 6):
+        self.ciphertext = ciphertext
+        self.length     = length
+        self.wordSet    = {x for x in self.ciphertext.split() if x.isalpha()}
+        
+        self.letterDict = self.getLetterDict(self.ciphertext)
+        self.init_key   = self.getInitKey(self.letterDict)
+        
+        
+    def getLetterDict(self, ciphertext):
+        letterDict = {x: 0 for x in self.P}
+        
+        for x in ciphertext:
+            if x in letterDict:
+                letterDict[x] += 1
+                
+        return letterDict
+        
+    def getInitKey(self, letterDict):
+        letterList = sorted(letterDict.items(), key = lambda e : e[1] , reverse=True)
+        return str().join([x[0] for x in letterList])
+        
+    def check(self, trans):
+        table = str.maketrans(trans, self.perhaps)
+        T = enchant.Dict('en_US')
+        
+        accept_count = 0
+        for x in self.wordSet:
+            if T.check(x.translate(table)):
+                accept_count += 1
+            
+        return accept_count
+                
+    
+    def upgrade(self, strs_start, strs_middle, strs_end):
+        best_middle = str().join(sorted(strs_middle))
+        best_accept = 0
+        
+        for x in itertools.permutations(sorted(strs_middle)):
+            new_middle = str().join(x)
+            accepts = self.check(strs_start + new_middle + strs_end)
+            if accepts > best_accept:
+                best_accept = accepts
+                best_middle = new_middle
+                self.logSuccess(best_middle, accepts)
+                
+        return best_middle
+        
+    def autoDecrypt(self):
+        print('AutoDecrypt start......')
+        
+        best_pwd  = self.init_key
+        
+        for i in range(len(self.init_key) - self.length + 1):
+            tstrs_start     = best_pwd[0: i]
+            tstrs_middle    = best_pwd[i: i + self.length]
+            tstrs_end       = best_pwd[i + self.length: ]
+            
+            print('\nThe ' + str(i) + ' times:')
+            print(tstrs_start + ' ' + tstrs_middle + ' ' + tstrs_end)
+            
+            upgrade_middle = self.upgrade(tstrs_start, tstrs_middle, tstrs_end)
+            best_pwd = tstrs_start + upgrade_middle + tstrs_end
+            
+        return best_pwd
+        
+    def logSuccess(self, current_middle, accept_count):
+        succ = accept_count / len(self.wordSet)
+        print(current_middle + ' ' + str(succ))
+    
+    
 if __name__ == '__main__':
-	D = PanDecrypt()
-	D.run()
-	D.check_init_key()
-
-	pwd = D.auto_decrypt()
-	f = open('0x01-decrypt.txt', 'w')
-	f.write(D.perhaps + '\n')
-	f.write(pwd)
-	f.close()
-
+    f = open('0x01-ciphertext.txt', 'r')
+    ciphertext = f.read().lower()
+    f.close()
+    
+    D = PanDecrypt(ciphertext)
+    pwd = D.autoDecrypt()
+    f = open('0x01-decrypt.txt', 'w')
+    f.write(D.perhaps + '\n')
+    f.write(pwd)
+    f.close()
